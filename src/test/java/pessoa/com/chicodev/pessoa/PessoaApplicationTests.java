79e930d61b1fac096a813764315e6f166ad86050
chicodev <gusterlima@hotmail.com>
2025-11-24 01:48:50 -0300

    feat(main): add class main

---
+++ src/test/java/pessoa/com/chicodev/pessoa/PessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package pessoa.com.chicodev.pessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
