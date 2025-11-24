97405bf30d6b059d4e4b97ff7059ceb4b5285067
chicodev <gusterlima@hotmail.com>
2025-11-24 01:50:36 -0300

    feat(mapper): built the resource about get all body of person

--- src/main/java/pessoa/com/chicodev/pessoa/model/mapper/PessoaDadoCompletoMapper.java
+++ src/main/java/pessoa/com/chicodev/pessoa/model/mapper/PessoaDadoCompletoMapper.java
@@ -3,15 +3,37 @@ package pessoa.com.chicodev.pessoa.model.mapper;
 import org.springframework.stereotype.Component;
 import pessoa.com.chicodev.pessoa.model.dto.PessoaDadoCompletoDTO;
 import pessoa.com.chicodev.pessoa.model.resource.PessoaDadoCompletoResource;
-import pessoa.com.chicodev.pessoa.model.resource.PessoaDadoEnderecoResource;
 
 @Component
 public class PessoaDadoCompletoMapper {
 
+    private final PessoaDadoSensivelMapper sensivelMapper;
+    private final PessoaContatoMapper contatoMapper;
+    private final PessoaDadoEnderecoMapper enderecoMapper;
 
-    public PessoaDadoCompletoDTO paraDadoCompletoDTO (PessoaDadoCompletoResource pessoaDadoCompletoResource) {
+    public PessoaDadoCompletoMapper(PessoaDadoSensivelMapper sensivelMapper,
+                                    PessoaContatoMapper contatoMapper,
+                                    PessoaDadoEnderecoMapper enderecoMapper) {
+        this.sensivelMapper = sensivelMapper;
+        this.contatoMapper = contatoMapper;
+        this.enderecoMapper = enderecoMapper;
+    }
+
+    public PessoaDadoCompletoDTO paraDadoCompletoDTO(PessoaDadoCompletoResource pessoaDadoCompletoResource) {
+
         PessoaDadoCompletoDTO dadoCompletoDTO = new PessoaDadoCompletoDTO();
 
-        dadoCompletoDTO.getPessoaDadoContatoDTO(pessoaDadoCompletoResource.getPessoaContatoResource());
+        dadoCompletoDTO.setPessoaId(pessoaDadoCompletoResource.getPessoaId());
+
+        var sensivelRes = pessoaDadoCompletoResource.getPessoaDadoSensivelResource();
+        dadoCompletoDTO.setPessoaDadoSensivelDTO(sensivelMapper.paraDadoSensivelDTO(sensivelRes));
+
+        var contatoRes = pessoaDadoCompletoResource.getPessoaDadoContatoResource();
+        dadoCompletoDTO.setPessoaDadoContatoDTO(contatoMapper.paraContatoDTO(contatoRes));
+
+        var enderecoRes = pessoaDadoCompletoResource.getPessoaDadoEnderecoResource();
+        dadoCompletoDTO.setPessoaDadoEnderecoDTO(enderecoMapper.paraDadoEnderecoDTO(enderecoRes));
+
+        return dadoCompletoDTO;
     }
 }
